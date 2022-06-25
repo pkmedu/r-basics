5c48c22bc973d32f4ea2788491c874ee58d7fee9
Pradip Muhuri <PMuhuri@cloud.ccas.gwu.edu>
2022-06-25 09:16:09 -0400

    6/25/22 9:15 am

---
+++ ManipulatingData/create_factors.R
@@ -0,0 +1,15 @@
+setwd("C:/r-basics/Data")
+getwd()
+
+
+load ("class_x.rdata")
+class <-class_x
+
+class <- within(class, { gender_fac1 <- factor(sex,levels=c ('M','F'),
+                                           labels=c('Male', 'Female'))                                   
+})
+
+
+class$gender_fac2 <- factor(class$sex, labels=c('Male Student', 'Female Student'))
+
+class[order(class$gender_fac1), ]
