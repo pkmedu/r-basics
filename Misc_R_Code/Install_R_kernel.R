5e6a82a52b51c21ba45dfe1ff7675538cce488ef
Pradip Muhuri <PMuhuri@cloud.ccas.gwu.edu>
2022-08-20 01:15:18 -0400

    8/20/2022 1:15 AM

--- Misc_R_Code/Install_R_kernel.R
+++ Misc_R_Code/Install_R_kernel.R
@@ -1,4 +1,9 @@
-install.packages("devtools")
+
+# https://richpauloo.github.io/2018-05-16-Installing-the-R-kernel-in-Jupyter-Lab/
+# Then run R from within Anaconda Prompt in Admin mode with R.exe.
+# Run the following commands successively from the same prompt
+install.packages("devtools", type = "win.binary")
+#install.packages("devtools")
 install.packages('IRkernel')
 IRkernel::installspec()
 IRkernel::installspec(user = FALSE)
